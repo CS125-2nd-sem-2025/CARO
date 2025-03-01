a4144b212ccc805b35dda7f47f1fd8ec13ea434d
abet1 <aacaro@up.edu.ph>
2025-03-01 10:16:25 +0800

    added app.py file for exer3

---
+++ exer3/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import platform
+import socket
+
+app = Flask(__name__)
+
+@app.route('/os', methods=['GET'])
+def get_os():
+    os_name = platform.system()
+    return jsonify({'os': os_name})
+
+@app.route('/version', methods=['GET'])
+def get_version():
+    os_version = platform.version()
+    return jsonify({'version': os_version})
+
+@app.route('/ip', methods=['GET'])
+def get_ip():
+    client_ip = request.remote_addr
+    return jsonify({'ip': client_ip})
+
+@app.route('/all', methods=['GET'])
+def get_all():
+    os_name = platform.system()
+    os_version = platform.version()
+    client_ip = request.remote_addr
+    return jsonify({
+        'os': os_name,
+        'version': os_version,
+        'ip': client_ip
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
